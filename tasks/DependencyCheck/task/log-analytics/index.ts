566cdde71eaa6559c3f1a704cc002f97e0ee6dd9
Craig Gumbley <craiggumbley@gmail.com>
2020-02-06 10:15:09 +0000

    Update to log analytics module

--- tasks/DependencyCheck/task/log-analytics/index.ts
+++ tasks/DependencyCheck/task/log-analytics/index.ts
@@ -1,5 +1,13 @@
 import * as crypto from 'crypto';
-import * as r from 'request-promise-native';
+import * as request from 'request-promise-native';
+
+interface ILogAnalyticsRequestHeaders {
+  'Content-Type': string;
+  Authorization: string;
+  'Log-Type': string;
+  'x-ms-date': string;
+  'time-generated-field'?: string;
+}
 
 export interface ILogAnalyticsResponse {
   name: string;
@@ -7,12 +15,13 @@ export interface ILogAnalyticsResponse {
   message: string;
 }
 
-export interface ILogAnalytics {
-  sendLogAnalyticsData(body: string, logType: string): Promise<ILogAnalyticsResponse>;
+export interface ILogAnalyticsClient {
+  sendLogAnalyticsData(
+    body: string, logType: string, timeGeneratedField?: string): Promise<ILogAnalyticsResponse>;
 }
 
-export class LogAnalytics implements ILogAnalytics {
-  constructor(private customerId: string, private sharedKey: string) {
+export class LogAnalyticsClient implements ILogAnalyticsClient {
+  constructor(private workspaceId: string, private sharedKey: string) {
 
   }
 
@@ -23,41 +32,50 @@ export class LogAnalytics implements ILogAnalytics {
     const encodedHash = crypto.createHmac('sha256', key)
       .update(string)
       .digest('base64');
-    return `SharedKey ${this.customerId}:${encodedHash}`;
+    return `SharedKey ${this.workspaceId}:${encodedHash}`;
   }
 
-  async sendLogAnalyticsData(body: string, logType: string): Promise<ILogAnalyticsResponse> {
+  async sendLogAnalyticsData(
+    body: string, logType: string, timeGeneratedField?: string,
+  ): Promise<ILogAnalyticsResponse> {
     const date: string = new Date().toUTCString();
     const sig: string = this.buildSignature(date, Buffer.byteLength(body, 'utf8'));
 
-    const headers = {
+    const headers: ILogAnalyticsRequestHeaders = {
       'Content-Type': 'application/json',
       Authorization: sig,
       'Log-Type': logType,
       'x-ms-date': date,
     };
 
+    if (timeGeneratedField) {
+      const isoPattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+      if (!isoPattern.test(timeGeneratedField)) {
+        throw new Error('The parameter [timeGeneratedField] should follow the ISO 8601 format (YYYY-MM-DDThh:mm:ssZ)');
+      }
+
+      headers['time-generated-field'] = timeGeneratedField;
+    }
+
     const options = {
-      url: `https://${this.customerId}.ods.opinsights.azure.com/api/logs?api-version=2016-04-01`,
+      url: `https://${this.workspaceId}.ods.opinsights.azure.com/api/logs?api-version=2016-04-01`,
       headers,
       body,
       resolveWithFullResponse: true,
     };
 
-    return r.post(options)
-      .then((response) => ({
-        name: 'Success',
-        stausCode: response.statusCode,
-        message: 'The Log Analytics operation was successful',
+    const response = await request.post(options)
+      .then((r) => ({
+        name: 'DataSubmitted',
+        stausCode: r.statusCode,
+        message: 'OK',
       }))
-      .catch((err) => {
-        const resErr = JSON.parse(err.response.body);
+      .catch((err) => ({
+        name: err.name,
+        stausCode: err.statusCode,
+        message: err.resonse ? err.response : JSON.parse(err.response.body),
+      }));
 
-        return {
-          name: resErr.Error,
-          stausCode: err.statusCode,
-          message: resErr.Message,
-        };
-      });
+    return response;
   }
 }
