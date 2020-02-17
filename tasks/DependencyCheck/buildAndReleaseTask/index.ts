946824adeedf0c87fe2a0a91048f49c2cb0ce66d
jack-education <jack>
2020-02-17 14:08:22 +0000

    Added ability to run on linux

--- tasks/DependencyCheck/buildAndReleaseTask/index.ts
+++ tasks/DependencyCheck/buildAndReleaseTask/index.ts
@@ -7,20 +7,17 @@ tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 const logType = 'DependencyCheck';
 
-
 var workspaceId : string = tl.getInput('workspaceId', true)!;
 var sharedKey: string = tl.getInput('sharedKey', true)!;
+
+var child_process = require('child_process');
 
 const la: ILogAnalyticsClient = new LogAnalyticsClient(
   workspaceId,
   sharedKey,
 );
 
-console.log('Before main async function')
-
 async function run(): Promise<void> {
-
-  console.log('In main async function')
 
   //Set variables for dependency-check-cli arguments - https://jeremylong.github.io/DependencyCheck/dependency-check-cli/arguments.html :
   /*
@@ -28,38 +25,31 @@ async function run(): Promise<void> {
   failOnCVSS: score set between 0 and 10
   */
 
+  if (process.platform === "linux") {
+    var cliScript = require.resolve('./dependency-check-cli/bin/dependency-check.sh');
+    var editPermission = child_process.spawnSync('chmod', ['+x', cliScript])
+  }
+  else if (process.platform === "win32") {
+    var cliScript = require.resolve('./dependency-check-cli/bin/dependency-check.bat');
+  }
+
   function owaspCheck() {
     const projectName = "OWASP Dependency Check"
     const format = "CSV"
 
     var scanPath : string = tl.getVariable('Agent.BuildDirectory')!;
-    console.log('ScanPath:')
-    console.log(scanPath)
 
     //Log warning if new version of dependency-check CLI is available
-
-
-    var argumentList = "--project".concat(projectName, " --scan ", scanPath, " --format ", format)
 
     //Pull cached files and save to dependency-check/data:
     //https://dependencycheck.sec540.com/data/jsrepository.json saved as jsrepository.json
     //https://dependencycheck.sec540.com/data/odc.mv.db saved as odc.mv.db
 
-    //Run dependency-check/bin/dependency-check.sh if Linux | dependency-check/bin/dependency-check.bat if Windows
 
-    var child_process = require('child_process');
-    var bat = require.resolve('./dependency-check-cli/bin/dependency-check.bat');
-
-    console.log(bat)
-
-    console.log('Before run of .bat file')
-    var report = child_process.spawnSync(bat, ['--project', projectName, '--scan', scanPath, '--format', format]);
-    console.log('After run of .bat file')
+    var report = child_process.spawnSync(cliScript, ['--project', projectName, '--scan', scanPath, '--format', format]);
   }
 
-  console.log('Before owaspCheck() function')
   owaspCheck()
-  console.log('After owaspCheck() function')
 
   const csvFilePath = './dependency-check-report.csv'
 
