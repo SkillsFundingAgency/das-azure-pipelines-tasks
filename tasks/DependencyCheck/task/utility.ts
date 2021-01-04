17e4343da3582d12d8761485aa45b3addece4191
jack-education <38191888+jack-education@users.noreply.github.com>
2021-01-04 11:08:54 +0000

    Removed warning for file deletion

--- tasks/DependencyCheck/task/utility.ts
+++ tasks/DependencyCheck/task/utility.ts
@@ -14,9 +14,7 @@ export function cleanDependencyCheckData(): void {
     tl.rmRF(p);
   } catch (e) {
     tl.debug(`An error was caught during cleanup ${e}`);
-    tl.warning(`Data path did not exist. The task will attempt to create it at: ${p}`);
   }
-
   tl.mkdirP(p);
 }
 
